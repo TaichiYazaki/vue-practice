faf990b1d4bc3c44e1b122c184747b945050c619
TaichiYazaki <taichi.yazaki@rakus-partners.co.jp>
2023-03-28 11:23:26 +0900

    firstcommit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,15 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <IfUsage />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import IfUsage from './components/IfUsage.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    IfUsage
   }
 }
 </script>
